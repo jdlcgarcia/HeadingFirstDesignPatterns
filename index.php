de90621173456520fe5813fe5edb5013511da80f
jdlcgarcia <kyomuga@gmail.com>
2019-10-06 11:56:27 +0200

    index to all the patterns

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Design Patterns First</title>
+    <meta name="description" content="Design Patterns First">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+<body>
+    <h1>Design Patterns</h1>
+    <ul>
+
+    </ul>
+</body>
+</html>
