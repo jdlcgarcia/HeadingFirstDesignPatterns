31a1384c4d082f49b913117d990840d0d9736d8e
jdlcgarcia <kyomuga@gmail.com>
2019-10-07 12:09:57 +0200

    Weather-O-Rama: test and autoload

--- observer/autoloader.php
+++ observer/autoloader.php
@@ -1,1 +1,7 @@
 <?php
+
+include_once "entity/Observer.php";
+include_once "entity/Subject.php";
+include_once "entity/WeatherData.php";
+include_once "entity/DisplayElement.php";
+include_once "entity/CurrentConditionsDisplay.php";
--- observer/index.php
+++ observer/index.php
@@ -1,3 +1,13 @@
 <?php
 
+use WeatherORama\entity\CurrentConditionsDisplay;
+use WeatherORama\Entity\WeatherData;
+
 include "autoloader.php";
+
+$weatherData = new WeatherData();
+$currentConditionsDisplay = new CurrentConditionsDisplay($weatherData);
+
+$weatherData->setMeasurements(80,65,30.4);
+$weatherData->setMeasurements(82,70,29.2);
+$weatherData->setMeasurements(78,90,29.2);
