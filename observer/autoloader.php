14c69d0995e42227592b5ad1e345b2a19c48c599
jdlcgarcia <kyomuga@gmail.com>
2019-10-07 13:22:03 +0200

    Weather-O-Rama: forecast display

--- observer/autoloader.php
+++ observer/autoloader.php
@@ -6,3 +6,4 @@ include_once "entity/WeatherData.php";
 include_once "entity/DisplayElement.php";
 include_once "entity/CurrentConditionsDisplay.php";
 include_once "entity/StatisticsDisplay.php";
+include_once "entity/ForecastDisplay.php";
--- observer/index.php
+++ observer/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use WeatherORama\entity\CurrentConditionsDisplay;
+use WeatherORama\entity\ForecastDisplay;
 use WeatherORama\entity\StatisticsDisplay;
 use WeatherORama\Entity\WeatherData;
 
@@ -9,6 +10,7 @@ include "autoloader.php";
 $weatherData = new WeatherData();
 $currentConditionsDisplay = new CurrentConditionsDisplay($weatherData);
 $statisticsDisplay = new StatisticsDisplay($weatherData);
+$forecastDisplay = new ForecastDisplay($weatherData);
 
 $weatherData->setMeasurements(80,65,30.4);
 $weatherData->setMeasurements(82,70,29.2);
